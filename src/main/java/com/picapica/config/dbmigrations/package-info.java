5ad7850d952c2f8c72747b72ad45fecc5f297a93
Anbit <extreme.ebooks.courses@gmail.com>
2020-02-29 19:21:27 +0000

    Initial version of reactdemo generated by JHipster-6.7.1

---
+++ src/main/java/com/picapica/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package com.picapica.config.dbmigrations;
---
+++ src/main/java/com/picapica/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,20 @@
+package com.picapica.security;
+
+import com.picapica.config.Constants;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT));
+    }
+}
