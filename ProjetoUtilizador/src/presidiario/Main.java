175a0a900785fa2cffc4fd0b253463a23b0d9bae
José Daniel Pereira <daniel_horacio1@hotmail.com>
2020-04-13 19:57:13 +0100

    inicio do front-end presidiário

---
+++ ProjetoUtilizador/src/presidiario/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        String url = "/com/jdenner/view/PrimeiraJanela.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(url));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
