4602631ead09d7a7430670114ac39fc5ffeec49c
José Daniel Pereira <57409867+danielPereira1989@users.noreply.github.com>
2020-04-20 18:12:16 +0100

    correct

--- ProjetoDai/src/sample/model/Quiz.java
+++ ProjetoDai/src/sample/model/Quiz.java
@@ -3,14 +3,23 @@ package sample.model;
 import java.util.ArrayList;
 
 public class Quiz {
-    public int idQuiz;
-    public int QtdQuestoes;
-    public ArrayList<Questoes> questoes= new ArrayList();
+    private int idQuiz;
+    private int QtdQuestoes;
+    private ArrayList<Questoes> questoes= new ArrayList();
 
 
     public Quiz(int idQuiz,int QtdQuestoes, ArrayList<Questoes> questoes){
         this.idQuiz=idQuiz;
         this.QtdQuestoes=QtdQuestoes;
         this.questoes=questoes;
+    }
+   public int getIdQuiz(){
+       return idQuiz;
+   }
+    public int getQtdQuestoes(){
+        return QtdQuestoes;
+    }
+    public ArrayList getQuestoes(){
+        return questoes;
     }
 }
