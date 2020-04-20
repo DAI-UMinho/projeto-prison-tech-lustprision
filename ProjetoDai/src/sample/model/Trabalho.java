5ac0193ab946ff9ee05a8eb8ce62d89401d9f14a
José Daniel Pereira <57409867+danielPereira1989@users.noreply.github.com>
2020-04-20 18:03:47 +0100

    correção

--- ProjetoDai/src/sample/model/Trabalho.java
+++ ProjetoDai/src/sample/model/Trabalho.java
@@ -1,11 +1,11 @@
 package sample.model;
 
 public class Trabalho {
-    public int IdTrabalho;
-    public String Nome;
-    public String Descricao;
-    public String PrecoHora;
-    public int Vagas;
+    private int IdTrabalho;
+    private String Nome;
+    private String Descricao;
+    private String PrecoHora;
+    private int Vagas;
 
     public Trabalho(int IdTrabalho,String Nome,String Descricao,String PrecoHora,int Vagas){
         this.IdTrabalho=IdTrabalho;
