623433e5f482f5f42f324936da999dc2f8a2a764
SamuelLopes420 <SamuelLopes420@users.noreply.github.com>
2020-04-20 17:50:13 +0100

    dwdw

--- ProjetoDai/src/sample/model/Questoes.java
+++ ProjetoDai/src/sample/model/Questoes.java
@@ -1,7 +1,14 @@
 package sample.model;
 
 public class Questoes {
+
     public int idQuestao;
     public String resposta;
     public int valorQuestao;
+
+    public Questoes(int idQuestao, String resposta , int valorQuestao){
+        this.idQuestao=idQuestao;
+        this.resposta=resposta;
+        this.valorQuestao=valorQuestao;
+    }
 }
--- ProjetoDai/src/sample/model/Quiz.java
+++ ProjetoDai/src/sample/model/Quiz.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class Quiz {
     public int idQuiz;
     public int QtdQuestoes;
-    public ArrayList<Questoes> x = new ArrayList();
+    public ArrayList<Questoes> questoes= new ArrayList();
 
+    public Quiz(int idQuiz,int QtdQuestoes, ArrayList<Questoes> questoes){
+        this.idQuiz=idQuiz;
+        this.QtdQuestoes=QtdQuestoes;
+        this.questoes=questoes;
+    }
 }
