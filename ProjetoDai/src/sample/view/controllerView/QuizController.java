761c7cb68ef1666ad4bebd677cbf584881b305d0
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2020-04-23 11:40:59 +0100

    .

--- ProjetoDai/src/sample/view/controllerView/QuizController.java
+++ ProjetoDai/src/sample/view/controllerView/QuizController.java
@@ -1,4 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package sample.view.controllerView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -6,55 +14,83 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
+/**
+ * FXML Controller class
+ *
+ * @author shenr
+ */
 public class QuizController implements Initializable {
+
+    public Label valortxt;
+
+    /**
+     * Initializes the controller class.
+     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
 
-    public void handlebtnLogout(ActionEvent actionEvent) throws IOException {
-        System.out.println("Botão Logout");
-        Parent logout_parent = FXMLLoader.load(getClass().getResource("/sample/view/Login.fxml"));
-
-        JOptionPane.showConfirmDialog(null, "Deseja fazer logout?", "Confirmação de Logout", JOptionPane.YES_NO_OPTION);
-
-        Scene logout_scene = new Scene(logout_parent);
-        Stage logout_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        logout_stage.setScene(logout_scene);
-        logout_stage.show();
+    public void handleBtnLoja(ActionEvent actionEvent) throws IOException {
+        System.out.println("Botão Loja");
+        Parent loja_parent = FXMLLoader.load(getClass().getResource("/sample/view/shop.fxml"));
+        Scene loja_scene = new Scene(loja_parent);
+        Stage loja_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        loja_stage.setScene(loja_scene);
+        loja_stage.show();
     }
 
-    public void handlebtnVoltar(ActionEvent actionEvent) throws IOException {
-        System.out.println("Botão Voltar");
-        Parent voltar_parent = FXMLLoader.load(getClass().getResource("/sample/view/HomePage.fxml"));
-        Scene voltar_scene = new Scene(voltar_parent);
-        Stage voltar_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        voltar_stage.setScene(voltar_scene);
-        voltar_stage.show();
+    public void handleBtnTrabalho(ActionEvent actionEvent) throws IOException {
+        System.out.println("Botão Trabalho");
+        Parent trabalho_parent = FXMLLoader.load(getClass().getResource("/sample/view/work.fxml"));
+        Scene trabalho_scene = new Scene(trabalho_parent);
+        Stage trabalho_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        trabalho_stage.setScene(trabalho_scene);
+        trabalho_stage.show();
     }
 
-    public void handlebuttonIniciar(ActionEvent actionEvent) throws IOException {
-        System.out.println("Botão Iniciar");
-        Parent iniciar_parent = FXMLLoader.load(getClass().getResource("/sample/view/Perguntas.fxml"));
-        Scene iniciar_scene = new Scene(iniciar_parent);
-        Stage iniciar_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        iniciar_stage.setScene(iniciar_scene);
-        iniciar_stage.show();
+    public void handleBtnQuiz(ActionEvent actionEvent) throws IOException {
+        System.out.println("Botão Quiz");
+        Parent quiz_parent = FXMLLoader.load(getClass().getResource("/sample/view/quiz.fxml"));
+        Scene quiz_scene = new Scene(quiz_parent);
+        Stage quiz_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        quiz_stage.setScene(quiz_scene);
+        quiz_stage.show();
     }
 
-    public void handlebuttonSair(ActionEvent actionEvent) throws IOException {
-        System.out.println("Botão Iniciar");
-        Parent iniciar_parent = FXMLLoader.load(getClass().getResource("/sample/view/login.fxml"));
-        Scene iniciar_scene = new Scene(iniciar_parent);
-        Stage iniciar_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        iniciar_stage.setScene(iniciar_scene);
-        iniciar_stage.show();
+    public void handleBtnPerfil(ActionEvent actionEvent) throws IOException {
+        System.out.println("Botão Perfil");
+        Parent perfil_parent = FXMLLoader.load(getClass().getResource("/sample/view/profile.fxml"));
+        Scene perfil_scene = new Scene(perfil_parent);
+        Stage perfil_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        perfil_stage.setScene(perfil_scene);
+        perfil_stage.show();
+    }
+
+    public void handleBtnSair(ActionEvent actionEvent) throws IOException {
+        System.out.println("Botão Perfil");
+        Parent perfil_parent = FXMLLoader.load(getClass().getResource("/sample/view/login.fxml"));
+        Scene perfil_scene = new Scene(perfil_parent);
+        Stage perfil_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        perfil_stage.setScene(perfil_scene);
+        perfil_stage.show();
+    }
+
+    public void onClickA1(ActionEvent actionEvent) {
+    }
+
+    public void onClickA3(ActionEvent actionEvent) {
+    }
+
+    public void onClickA2(ActionEvent actionEvent) {
+    }
+
+    public void onClickA4(ActionEvent actionEvent) {
     }
 }
