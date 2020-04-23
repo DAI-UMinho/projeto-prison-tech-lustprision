47c1c6b712fcbe1455125912c7f82ace0d19f23f
SamuelLopes420 <SamuelLopes420@users.noreply.github.com>
2020-04-23 16:36:26 +0100

    v

--- ProjetoDai/src/sample/controller/WORK_TB.java
+++ ProjetoDai/src/sample/controller/WORK_TB.java
@@ -3,7 +3,7 @@ package sample.controller;
 import sample.model.Trabalho;
 
 public class WORK_TB {
-    Trabalho type;
+    public Trabalho type;
 
     public WORK_TB(Trabalho type){
         this.type=type;
--- ProjetoDai/src/sample/view/controllerView/WorkLineController.java
+++ ProjetoDai/src/sample/view/controllerView/WorkLineController.java
@@ -4,20 +4,40 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
 
 public class WorkLineController implements Initializable{
+
+    @FXML public Pane workpane;
+    @FXML public Label worknamelbl;
+    @FXML public Label workvagaslbl;
+    @FXML public Label workremunlbl;
+
+    public static String name;
+    public static int vagas;
+    public static int remun;
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        worknamelbl.setText(name);
+        workvagaslbl.setText(Integer.toString(vagas));
+        workremunlbl.setText(Integer.toString(remun));
     }
+
+
+
 
 
 }
